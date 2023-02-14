8aa5178ad3855434566306254890234fa4a7e1ce
Beto Costa <jjbetocosta23@gmail.com>
2023-02-13 22:21:49 -0300

    função de soma e de subtração

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
-const robotron = document.querySelector(#robotron)
+const subtrair = document.querySelector("#subtrair")
+const somar = document.querySelector("#somar")
+const braco = document.querySelector("#braco")
 
-robotron.addEventListener("click", console.log("oi"))
+somar.addEventListener("click", (evento) =>{
+    braco.value = parseInt(braco.value) + 1
+})
 
-function dizOi() {
-    console.log("oi")
-    console.log("Bem-vindo ao Robotron 2000")
-}
-
-dizOi()
+subtrair.addEventListener("click", (evento) =>{
+    braco.value = parseInt(braco.value) - 1
+})
